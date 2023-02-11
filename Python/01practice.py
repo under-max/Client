96bfc6e3dc77854fdbcb295698904a6d3561c97b
under-max <118119897+under-max@users.noreply.github.com>
2023-02-11 18:58:20 +0900

    2.11

---
+++ Python/01practice.py
@@ -0,0 +1,32 @@
+print("hello!")
+
+a = 2
+b = 3
+c = a + b
+print(c)
+
+d = "Hello"
+print(d)
+
+e = False
+f = True
+
+dead = True
+Alive = False 
+
+print("Hello", a ,"your also", d)
+
+print(12)
+print("Hello")
+print(True, "HEy")
+
+def say_Hello():
+    print("Who are you")
+
+say_Hello()
+
+def say_goodbye():
+    print("Bye~")
+    say_Hello()
+    
+say_goodbye()
