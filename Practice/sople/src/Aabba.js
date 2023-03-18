70ec2101b00204ea380050b1043a89466c2fa54a
under-max <saigrams@naver.com>
2023-03-18 23:29:44 +0900

    Create Aabba.js

---
+++ Practice/sople/src/Aabba.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Aabba = () => {
+    return(
+        <div>
+            <h1>현재시간 {new Date().toLocaleTimeString()}</h1>
+        </div>
+    )
+}
+
+export default Aabba
